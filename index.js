c0d764218f4ae9c786790f54c86d54bb0fb53a62
Moloy das <classicitdevteam@gmail.com>
2024-01-17 11:00:01 +0600

    init commit

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
+// const port = process.env.PORT || 8080;
+const port = 5000 || 8080;
 const httpStatus = require("http-status");
 const connectDB = require("./server");
-const routes = require('./src/routes/routes');
+const routes = require("./src/routes/routes");
 const globalErrorHandler = require("./src/middleware/global.error.handler");
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -16,8 +21,7 @@ app.get("/", (req, res) => {
 });
 
 // Import All Api
-app.use('/api/v1', routes);
-
+app.use("/api/v1", routes);
 
 //global error handler
 app.use(globalErrorHandler);
@@ -26,27 +30,21 @@ app.use(globalErrorHandler);
 app.use((req, res, next) => {
   res.status(httpStatus.NOT_FOUND).json({
     success: false,
-    message: 'Not Found',
+    message: "Not Found",
     errorMessages: [
       {
         path: req.originalUrl,
-        message: 'API Not Found',
+        message: "API Not Found",
       },
     ],
   });
   next();
 });
 
-
 connectDB();
-
-// const port = process.env.PORT || 8080;
-const port = 5000 || 8080;
 
 app.listen(port, () => {
   console.log(`Ecommerce app is running on port ${port}`);
 });
-
-
 
 // index.js -> routes -> route -> controllers -> services -> models.
